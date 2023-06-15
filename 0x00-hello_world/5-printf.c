790650c7fd687b7e6afaf18f7114fde56af4c38c
cliffton8052 <cliffton8052@gmail.com>
2023-06-15 12:11:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program of code that uses prinf to output a command
+ * Return: 0 (Pass)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

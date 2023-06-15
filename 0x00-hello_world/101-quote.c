b2ea4762d5eff76236f74d656c809cea3a0d0ee4
cliffton8052 <cliffton8052@gmail.com>
2023-06-15 14:46:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: print a quote using write function write function 
- * Return: 1 (fail)
+ * Description: print a quote using write function.
+ * Return: 1 (error).
  */
-
 int main(void)
 {
-char wycliff[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-write(1,wycliff,30);
-return(1);
+char wyc[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, wyc, 30);
+return (1);
 }

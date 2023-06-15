ea7e3e3121ff25bf11b8a5e2caef93c8bd5d0885
cliffton8052 <cliffton8052@gmail.com>
2023-06-15 15:40:30 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 /**
- * main -  C program that prints the size of bytes.
+ * main - Entry point
+ * Description: C programming that prints size of bytes
  * Return 0 (Pass)
  */
-
 int main(void)
 {
-char c;
-int i;
-long int x;
-long long int z;
-float f;
-
+	char c;
+	int i;
+	long int x;
+	long long int z;
+	float f;
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(int));
-printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(long long int));
-printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(float));
+printf("Size of long int: %lu byte(s)\n" (unsigned long)sizeof(long int));
+printf("Size of long long int: %lu byte(s)\n" (unsigned long)sizeof(long long int));
+printf("Size of float %lu byte(s)\n" (unsigned long)sizeof(float));
 return (0);
 }

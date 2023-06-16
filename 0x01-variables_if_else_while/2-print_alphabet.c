75f7d5822fe62223c321ee479f50b1cd24d86f1a
cliffton8052 <cliffton8052@gmail.com>
2023-06-16 16:07:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - programs point of entry
+ * Description - 2-print_alphabet.c
+ * Return: 0 (Pass)
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);

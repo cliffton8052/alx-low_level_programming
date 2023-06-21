02ebc595ca503d8f6f289045b314b282ce2b2bb9
cliffton8052 <cliffton8052@gmail.com>
2023-06-20 17:18:56 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point of program
+ * Description: computes the sum and multiples
+ * of three and 5
+ * Return: 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);

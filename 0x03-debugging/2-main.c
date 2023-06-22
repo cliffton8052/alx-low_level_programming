41d2206adbf8bbf92bfea108446a7eea8e9441e6
cliffton8052 <cliffton8052@gmail.com>
2023-06-22 05:24:56 -0700

    3-print_remaining_days.c

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -16,8 +16,9 @@ int main(void)
 	c = 0;
 
 	largest = largest_number(a, b, c);
-
-	printf("%d is the largest number\n", largest);
+	{
+		printf("%d is the largest number\n", largest);
+	}
 
 	return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,20 +1,32 @@
-#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 /**
- * positive_or_negative - prints a positive or a negative number
- * @n - positive or negative number
- * Return: 0 (Success)
+ * positive_or_negative - checks if a number is positive or negative.
+ * @i - the number to be checked
+ * Return: 1
  */
 
-void positive_or_negative(int n);
+void positive_or_negative(int i)
 {
+	int i;
 
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n == 0)
-		printf("%i is zero\n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+
+		printf("%d is zero\n", i);
+	}
 	else
-		printf("%i is negative\n", n);
+	{
+		printf("%d is negative\n", i);
+	}
 
-	return (0);
+	return (i);
 }

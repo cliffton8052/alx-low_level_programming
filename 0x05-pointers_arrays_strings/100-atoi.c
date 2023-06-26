97944ef904c4193a3e51d78d6e6ac6356be7d3fc
cliffton8052 <cliffton8052@gmail.com>
2023-06-26 13:47:16 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,29 +8,18 @@
 
 int _atoi(char *s)
 {
-	int c = 0;
 	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int num = 1;
 
-	while (s[c])
-	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-		if (isi == 1)
-		{
-			break;
-		}
-		c++;
-	}
-	ni *= min;
-	return (ni);
-}
+	do {
+		if (*s == '-')
+			num *= -1;
+		else if (*s >= '0' && *s <= '9')
+				ni = (ni * 10) + (*s - '0');
+
+				else if (ni > 0)
+				break;
+				} while (*s++);
+
+				return (ni * num);
+				}

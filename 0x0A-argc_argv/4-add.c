916010d78266c609f138f07a53c4717a12e1d299
cliffton8052 <cliffton8052@gmail.com>
2023-07-07 06:55:54 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,9 +17,9 @@ int main(int argc, char *argv[])
 	{
 		for (c = argv[argc]; *c; c++)
 			if (*c < '0' || *c > '9')
-				return (printf("Error\n), 1);
+				return (printf("Error\n"), 1);
 		sum = sum + atoi(argv[argc]);
 	}
-	printf("%d\n", sum);
+	printf(" % d\n", sum);
 	return (0);
 }

f44213b2e77b0d6c5f8e4c22db9d56a8b8b56ff8
cliffton8052 <cliffton8052@gmail.com>
2023-07-11 05:08:48 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,7 +10,8 @@ int _strlen(char *s)
 {
 	int size = 0;
 
-	for (; s[size] != '\0'; size++);
+	for (; s[size] != '\0'; size++)
+		;
 
 	return (size);
 }
@@ -27,7 +28,7 @@ char *argstostr(int ac, char **av)
 	int i = 0, nc = 0, j = 0, cmpt = 0;
 	char *s;
 
-	if (ac == 0 || ac == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (; i < ac; i++, nc++)

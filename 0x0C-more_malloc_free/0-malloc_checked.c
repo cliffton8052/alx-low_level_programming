f366eca915bfa9dcfda19221e8ab425e6e2afc37
cliffton8052 <cliffton8052@gmail.com>
2023-07-13 07:08:15 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - function that allocates memory using malloc
+ * @b: int
+ * Return: pointer to the array initialized or NULL
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+	return (m);
+}
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes character to stdout
+ * @c: character to be printed
+ * Return: 1 on success
+ * and -1 on error
+ */
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}

a8e0898104fce015cae42b484eb2e7809d2738b8
cliffton8052 <cliffton8052@gmail.com>
2023-07-17 04:47:23 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure to store information about a dog
+ * @name: a pointer to a character (string) representing the dog's name
+ * @age: a float representing the dog's age
+ * @owner: a pointer to a character (string) representing the owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */

3ed0300e5314427470fbe574a04188ed48af2413
cliffton8052 <cliffton8052@gmail.com>
2023-07-31 13:58:21 -0700

    listint safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,42 +1,46 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Print a listint_t linked list safely.
- * @head: A pointer to the head of the list.
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: Pointer to the head of the linked list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t node_count = 0;
-	const listint_t *current = head;
-	const listint_t **address_table = malloc(sizeof(listint_t *) * 1024);
+	const listint_t *slow = head;
+	const listint_t *fast = head;
+	size_t count = 0;
 
-	if (address_table == NULL)
+	while (fast && fast->next)
 	{
-		printf("Memory allocation failed\n");
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			count++;
+			break;
+		}
+	}
+
+	if (slow == fast && fast != NULL)
+	{
+		printf("-> [%p] %d\n", (void *)fast, fast->n);
 		exit(98);
 	}
 
-	while (current != NULL)
+	while (slow != NULL)
 	{
-		size_t index = (size_t)current % 1024;
-
-		if (address_table[index] == current)
-		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			printf("Loop detected!\n");
-			free(address_table);
-			exit(98);
-		}
-
-		printf("[%p] %d\n", (void *)current, current->n);
-		address_table[index] = current;
-		current = current->next;
-		node_count++;
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+		slow = slow->next;
 	}
 
-	free(address_table);
-	return (node_count);
+	return (count);
 }
 

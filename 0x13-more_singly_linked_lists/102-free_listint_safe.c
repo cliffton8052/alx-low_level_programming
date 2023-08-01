793cd73c0c39fa98399b47db87801fb36783787d
cliffton8052 <cliffton8052@gmail.com>
2023-08-01 12:14:19 -0700

    free, safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,29 @@
 #include "lists.h"
+#include <stdlib.h>
+
+/**
+ * find_listint_loopfl - finds a loop in linked list
+ * @head: linked list to search
+ *
+ * Return: address of node where loop starts/returns NULL
+ */
+listint_t *find_listint_loop_fl(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
 
 /**
  * free_listint_safe - Frees a listint_t linked list safely.

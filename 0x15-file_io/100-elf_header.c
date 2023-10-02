750b8116b6d0f5ccf0c3bfaf5c8b3842f3b556ed
cliffton8052 <okumuwycliff8052@gmail.com>
2023-10-02 12:49:52 -0700

    100-elf_header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -18,12 +18,12 @@ void print_elf_header_info(Elf64_Ehdr *header)
 	printf("\n");
 
 	printf("Class: %s\n", (header->e_ident[EI_CLASS] == ELFCLASS32)
-		       	? "ELF32" : "ELF64");
+			? "ELF32" : "ELF64");
 	printf("Data: %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB)
-		       	? "2's complement, little endian" : "Unknown");
+			? "2's complement, little endian" : "Unknown");
 	printf("Version: %d\n", header->e_ident[EI_VERSION]);
 	printf("OS/ABI: %s\n", (header->e_ident[EI_OSABI] == ELFOSABI_SYSV)
-		       	? "UNIX - System V" : "Unknown");
+			? "UNIX - System V" : "Unknown");
 	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
 
 	const char *type_str;
@@ -46,22 +46,29 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
-		exit(98);
+		exit(EXIT_FAILURE);
 	}
 
 	int fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
+	{
 		print_error("Error: Cannot open file");
+		exit(EXIT_FAILURE);
+	}
 
 	Elf64_Ehdr header;
 	ssize_t bytes_read = read(fd, &header, sizeof(header));
 	if (bytes_read != sizeof(header))
+	{
 		print_error("Error: Cannot read ELF header");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
 
 	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1]
-		       	!= ELFMAG1 ||
+			!= ELFMAG1 ||
 			header.e_ident[EI_MAG2] != ELFMAG2 ||
-		       	header.e_ident[EI_MAG3] != ELFMAG3)
+			header.e_ident[EI_MAG3] != ELFMAG3)
 		print_error("Error: Not an ELF file");
 
 	print_elf_header_info(&header);

112b69ea81fc9c9b288ec49cd7e49ac0587ed46f
cliffton8052 <okumuwycliff8052@gmail.com>
2023-10-02 13:13:08 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,74 +4,68 @@
 #include <fcntl.h>
 #include <elf.h>
 
-void print_error(const char *msg)
-{
-	dprintf(STDERR_FILENO, "%s\n", msg);
-	exit(98);
-}
+/**
+ * print_error - Prints an error message to the standard error stream.
+ * @message: The error message to be printed.
+ *
+ * This function prints an error message to the standard error stream
+ * along with a newline character and is typically used to report errors
+ * that occur during program execution.
+ *
+ * Example usage:
+ *   print_error("Error: Cannot open file");
+ *
+ * @message: The error message to be printed.
+ */
 
 void print_elf_header_info(Elf64_Ehdr *header)
 {
-	printf("Magic: ");
-	for (int i = 0; i < EI_NIDENT; i++)
+	int i;
+	const char *type_str;
+
+	printf("ELF Header:\n");
+	printf("  Magic: ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
 		printf("%02x ", header->e_ident[i]);
+	}
 	printf("\n");
 
-	printf("Class: %s\n", (header->e_ident[EI_CLASS] == ELFCLASS32)
-			? "ELF32" : "ELF64");
-	printf("Data: %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB)
-			? "2's complement, little endian" : "Unknown");
-	printf("Version: %d\n", header->e_ident[EI_VERSION]);
-	printf("OS/ABI: %s\n", (header->e_ident[EI_OSABI] == ELFOSABI_SYSV)
-			? "UNIX - System V" : "Unknown");
-	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
-
-	const char *type_str;
-	switch (header->e_type)
-	{
-		case ET_NONE: type_str = "None"; break;
-		case ET_REL: type_str = "REL"; break;
-		case ET_EXEC: type_str = "EXEC"; break;
-		case ET_DYN: type_str = "DYN"; break;
-		case ET_CORE: type_str = "CORE"; break;
-		default: type_str = "Unknown";
-	}
-	printf("Type: %s\n", type_str);
-
-	printf("Entry point address: %#lx\n", (unsigned long)header->e_entry);
 }
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 on success, EXIT_FAILURE on failure.
+ */
 
 int main(int argc, char *argv[])
 {
+	int fd;
+	Elf64_Ehdr header;
+
 	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "Usage: %s <file_name>\n", argv[0]);
+		return (EXIT_FAILURE);
 	}
 
-	int fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
-		print_error("Error: Cannot open file");
-		exit(EXIT_FAILURE);
+		perror("Error: Cannot open file");
+		return (EXIT_FAILURE);
 	}
 
-	Elf64_Ehdr header;
 	ssize_t bytes_read = read(fd, &header, sizeof(header));
-	if (bytes_read != sizeof(header))
+	if (bytes_read == -1)
 	{
-		print_error("Error: Cannot read ELF header");
+		perror("Error: Cannot read ELF header");
 		close(fd);
-		exit(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-
-	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1]
-			!= ELFMAG1 ||
-			header.e_ident[EI_MAG2] != ELFMAG2 ||
-			header.e_ident[EI_MAG3] != ELFMAG3)
-		print_error("Error: Not an ELF file");
-
-	print_elf_header_info(&header);
 
 	close(fd);
 	return (0);
